f4d14ad88e4e5924092fd19fd378914d2a987e73
iGeronimo <mathijsL@hotmail.com>
2023-01-24 00:27:23 +0300

    Removed doubles

--- mge_v19_student_version_as4 - Copy/mge_v19_student_version/src/mge/config.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-namespace config {
-    const std::string MGE_MODEL_PATH ("mge/models/");
-    const std::string MGE_TEXTURE_PATH ("mge/textures/");
-    const std::string MGE_SHADER_PATH ("mge/shaders/");
-    const std::string MGE_FONT_PATH ("mge/fonts/");
-    const std::string MGE_TERRAIN_PATH("mge/terrain/");
-}
